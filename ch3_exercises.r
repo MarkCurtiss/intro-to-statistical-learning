994c18c5f6966bb81547c4c520894506f68c3520
Mark Curtiss <markcurtiss@gmail.com>
2019-03-10 21:08:55 -0700

    Worked questions 9-14

--- ch3_exercises.r
+++ ch3_exercises.r
@@ -36,9 +36,9 @@ salary(gpa=4.0,iq=110,gender=1)
 # I don't think there's enough information to tell, since we don't know
 # whether the true relationship is closer to linear or cubic.
 
-# 5.??  How am I supposed to make a y-sub-i-tilde
-
-# 6. ??
+# 5. *TODO*  How am I supposed to make a y-sub-i-tilde
+# 6. *TODO*
+# 7. My study group is skipping this one.
 
 # 8.
 # a)
@@ -64,7 +64,7 @@ abline(regression, lwd=3, col="red")
 #    The studentized residuals show that we have 3 outliers.
 
 # 9)
-# a) 
+# a)
 plot(Auto)
 # b)
 cor(Auto[,1:8])
@@ -105,7 +105,7 @@ plot(predict(fit), rstudent(fit))
 # The slight u-shape in the residuals vs fitted plot suggests mild non-linearity in the data.
 # There's one observation with very high leverage.
 # There are a couple of points that are outliers: |rstudent| > 3 (in one case, 4!)
-# 
+#
 # e)
 interact_fit = lm(mpg~cylinders*horsepower+
         displacement*weight+
@@ -126,13 +126,12 @@ summary(nonlinear_fit)
 # from .8215 to .8671
 
 # 10.
-
 # a)
 library(ISLR)
 attach(Carseats)
 carseat.regression <- lm(Sales~Price+Urban+US)
 
-# b) 
+# b)
 summary(carseat.regression)
 # Increases in price decrease sales.
 # Less carseats are sold in urban areas.
@@ -158,5 +157,196 @@ plot(carseat.limited_regression)
 # I also see two studentized residuals close to 3.
 
 #11.
+# a)
+set.seed(1)
+x=rnorm(100)
+y=2*x+rnorm(100)
+no_intercept <- lm(y~x+0)
+summary(no_intercept)
+# The coefficient estimate is 1.9939, standard error is 0.1065, t-value is
+# 18.73 and the p-value is <2e-16.  The high t-value and low p-value lead me
+# to reject the null hypothesis.
 
+# b)
+another_non_intercept <- lm(x~y+0)
+summary(another_non_intercept)
+# The coefficient is 0.39111, standard error is 0.02089, t-value is
+# 18.73 and the p-value is <2e-16.  The high t-value and low p-value lead me
+# to reject the null hypothesis.
 
+# c) The coefficients and p-values are exactly the same.
+# d) *TODO*
+# e) *TODO*
+# f)
+x_on_y <- lm(y~x)
+y_on_x <- lm(x~y)
+summary(x_on_y)
+summary(y_on_x)
+# t-value = 18.556
+
+# 12.
+# a) When there's a perfectly 1:1 relationship between the two.
+# b)
+set.seed(1)
+x=rnorm(100)
+y=2*x+rnorm(100)
+x_on_y <- lm(y~x+0)
+y_on_x <- lm(x~y+0)
+summary(x_on_y)
+summary(y_on_x)
+# coefficient for x_on_y = 1.9939, coefficient for y_on_x = 0.39111
+
+#c)
+set.seed(1)
+x <- rnorm(100)
+y <- -sample(x, 100)
+x_on_y <- lm(y~x+0)
+y_on_x <- lm(x~y+0)
+summary(x_on_y)
+summary(y_on_x)
+# coefficient for x_on_y = coefficient for y_on_x = -0.02148
+
+#13.
+# a)
+set.seed(1)
+x <- rnorm(100,mean=0,sd=1)
+
+# b)
+eps <- rnorm(100, mean=0, sd=0.25)
+
+# c)
+y <- -1 + (.5*x) + eps
+length(y)
+# 100
+# B0 = -1, B1 = 0.5
+
+# d)
+plot(x,y)
+# Most observations cluster around 0 which makes sense given the mean was set to 0 for both
+# X and Y.
+
+# e)
+lm.fit <- lm(y~x)
+summary(lm.fit)
+# B^0 and B^1 are really close! -1.00942 and 0.49973, respectively
+
+# f)
+plot(x,y)
+abline(lm.fit, lwd=3, col='red')
+legend(x="top", legend="Simulated X vs Y with linear regression")
+
+# g)
+quad.fit <- lm(y~x+I(x^2))
+plot(x,y)
+abline(quad.fit, lwd=3, col="red")
+# No.  Not only does the fit look the same, but the RSE and R^2 values are nearly the same.
+
+# h)
+set.seed(1)
+x <- rnorm(100,mean=0,sd=1)
+eps <- rnorm(100, mean=0, sd=0.1)
+y <- -1 + (.5*x) + eps
+lm.fit <- lm(y~x)
+quad.fit <- lm(y~x+I(x^2))
+# The linear regressions fit the data even more closely, with R^2 values of 0.9565.
+# The t-value and p-values for the quadratic fit don't look great either.
+
+# i)
+set.seed(1)
+x <- rnorm(100,mean=0,sd=1)
+eps <- rnorm(100, mean=0, sd=10)
+y <- -1 + (.5*x) + eps
+lm.fit <- lm(y~x)
+quad.fit <- lm(y~x+I(x^2))
+plot(x,y)
+abline(lm.fit, col="red", lwd=3)
+abline(quad.fit, col="blue", lwd=3)
+summary(lm.fit)
+summary(quad.fit)
+anova(lm.fit, quad.fit)
+# The quadratic fit does a better job fitting this noisier data.
+# The RSE is lower and the R^2 is an order of magnitude larger.
+
+# j)
+set.seed(1)
+x <- rnorm(100,mean=0,sd=1)
+eps <- rnorm(100, mean=0, sd=0.25)
+y <- -1 + (.5*x) + eps
+lm.fit <- lm(y~x)
+confint(lm.fit)
+#                  2.5 %     97.5 %
+# (Intercept) -1.0575402 -0.9613061
+# x            0.4462897  0.5531801
+
+set.seed(1)
+x <- rnorm(100,mean=0,sd=1)
+eps <- rnorm(100, mean=0, sd=0.1)
+y <- -1 + (.5*x) + eps
+lm.fit <- lm(y~x)
+confint(lm.fit)
+#                  2.5 %     97.5 %
+# (Intercept) -1.0230161 -0.9845224
+# x            0.4785159  0.5212720
+
+set.seed(1)
+x <- rnorm(100,mean=0,sd=1)
+eps <- rnorm(100, mean=0, sd=10)
+y <- -1 + (.5*x) + eps
+lm.fit <- lm(y~x)
+confint(lm.fit)
+#                 2.5 %    97.5 %
+# (Intercept) -3.301607 0.5477552
+# x           -1.648412 2.6272040
+
+# 14.
+# a)
+set.seed(1)
+x1 <- runif(100)
+x2 <- 0.5*x1+rnorm(100)/10
+y <- 2+2*x1+0.3*x2+rnorm(100)
+# B0 = 2, B1 = 2, B2 = .03
+
+# b) x2 is ~ .5*x1
+plot(x1, x2)
+
+# c)
+lm.fit <- lm(y~x1+x2)
+summary(lm.fit)
+# B^0 = 1.9144, B^1 = 1.9604, B^2 = 0.5485
+# B^0 and B^1 are close to the true predictors but B^2 is off.
+# I can maybe reject the null hypothesis that B1 = 0 due to the p.value of 0.0146.
+# I cannot reject the null hypothesis that B2 = 0.
+
+# d)
+lm.fit <- lm(y~x1)
+summary(lm.fit)
+# This is a much better fit.  I can reject the null hypothesis B1 = 0 due ot the
+# tiny p-value.
+
+# e)
+lm.fit <- lm(y~x2)
+summary(lm.fit)
+# I can reject the null hypothesis that B1 = 0 due to the tiny p-value.
+
+# f) No, the results demonstrate that the predictors are collinear.
+
+# g)
+x1 <- c(x1, 0.1)
+x2 <- c(x2, 0.8)
+y <- c(y,6)
+lm.fit <- lm(y~x1+x2)
+plot(lm.fit)
+hatvalues(lm.fit)[100] / mean(hatvalues(lm.fit))
+# This is not an outlier or a high-leverage point in this model, based on the plot
+# and also the fact that our predicted value isn't much greater than the average
+# prediction.
+lm.fit <- lm(y~x1)
+plot(lm.fit)
+tail(hatvalues(lm.fit), 1) / mean(hatvalues(lm.fit))
+# This is an outlier in this model due to it being a > 3 studentized residual.
+# It's not quite large enough to be a high-leverage point, though.
+lm.fit <- lm(y~x2)
+plot(lm.fit)
+tail(hatvalues(lm.fit), 1) / mean(hatvalues(lm.fit))
+# This is a high leverage point in this model - it's 4.372822 times greater than
+# the average hatvalue!
