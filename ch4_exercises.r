24a79c926065a71743306f9b0a38043e1c1f3250
Mark Curtiss <markcurtiss@gmail.com>
2019-03-21 18:31:13 -0700

    Start on the applied problems in ch.4

--- ch4_exercises.r
+++ ch4_exercises.r
@@ -43,5 +43,56 @@ receive_an_a(40, 3.5)
 # error rate of 1% and a test error rate of 35%.
 
 # 9.
-# a)
+# a) 37/100 ?
 # b) 4 in 25.
+
+# 10.
+require(ISLR)
+plot(Weekly)
+# a) Volume increases by year.  I don't see other obvious patterns.
+
+log.weekly <- glm(Direction~.-Today-Year,family="binomial",data=Weekly)
+# b) The Lag2 variable is statistically significant (p-value 3%) but not
+# enough that I'd want to try and trade on it!
+
+pct_of_correct_predictions <- function(confusion_matrix) {
+    (
+        (confusion_matrix[1,1] + confusion_matrix[2,2]) / sum(confusion_matrix)
+    ) * 100
+}
+
+log.probs <- predict(log.weekly,type="response")
+log.predictions <- rep("Down", dim(Weekly)[1])
+log.predictions[log.probs > 0.5] = "Up"
+confusion_matrix <- table(log.predictions, Weekly$Direction, dnn=c('predictions', 'actual'))
+confusion_matrix
+pct_of_correct_predictions(confusion_matrix)
+mean(log.predictions==Weekly$Direction) * 100
+
+# c)
+#            Weekly
+# predictions Down  Up
+#        Down   54  48
+#        Up    430 557
+# 56.10652
+# Logistic regression does better on days when it guesses 'Up', with a 56% success rate
+# vs a 53% success rate on the days when it guesses 'Down'.
+
+train <- (Weekly$Year < 2009)
+training_data <- Weekly[train, ]
+test_data <- Weekly[!train,]
+
+log.weekly_trained <- glm(Direction~Lag2,family="binomial",data=Weekly,subset=train)
+log.trained_probs <- predict(log.weekly_trained,test_data,type="response")
+log.trained_predictions <- rep("Down", dim(test_data)[1])
+log.trained_predictions[log.trained_probs > 0.5] = "Up"
+confusion_matrix <- table(log.trained_predictions, test_data$Direction, dnn=c('predictions', 'actual'))
+confusion_matrix
+pct_of_correct_predictions(confusion_matrix)
+
+# d)
+#            actual
+# predictions Down Up
+#        Down    9  5
+#        Up     34 56
+# 62.5
