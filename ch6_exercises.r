ef5810d83ff02f175370d01555f41367a8d6cefc
Mark Curtiss <markcurtiss@gmail.com>
2019-04-30 21:24:40 -0700

    Start working exercise 9

--- ch6_exercises.r
+++ ch6_exercises.r
@@ -1,55 +1,6 @@
 install.packages('leaps')
 library('leaps')
 
-Hitters=na.omit(Hitters)
-regfit.full <- regsubsets(Salary~.,Hitters)
-summary(regfit.full)
-
-regfit.full <- regsubsets(Salary~.,Hitters,nvmax=19)
-reg.summary <- summary(regfit.full)
-reg.summary$rsq
-par(mfrow=c(2,2))
-plot(reg.summary$rss, xlab="Number of variables", ylab="RSS", type="l")
-plot(reg.summary$adjr2,xlab="Number of variables",ylab="Adjusted RSq", type="l")
-which.max(reg.summary$adjr2)
-points(11,reg.summary$adjr2[11],col="red",cex=2,pch=20)
-plot(reg.summary$cp, xlab="Number of variables", ylab="Cp", type="l")
-which.min(reg.summary$cp)
-points(10,reg.summary$cp[10],col="red",cex=2,pch=20)
-which.min(reg.summary$bic)
-plot(reg.summary$bic,xlab="Number of variables", ylab="BIC", type="l")
-points(6, reg.summary$bic[6],col="red",cex=2,pch=20)
-plot(regfit.full,scale="r2")
-plot(regfit.full,scale="adjr2")
-plot(regfit.full,scale="Cp")
-plot(regfit.full,scale="bic")
-coef(regfit.full,6)
-
-regfit.fwd <- regsubsets(Salary~.,data=Hitters,nvmax=19,method="forward")
-summary(regfit.fwd)
-regfit.bwd <- regsubsets(Salary~.,data=Hitters,nvmax=19,method="backward")
-summary(regfit.bwd)
-
-coef(regfit.full,7)
-coef(regfit.fwd,7)
-coef(regfit.bwd,7)
-
-set.seed(1)
-train <- sample(c(TRUE,FALSE), nrow(Hitters), rep=TRUE)
-test <- (!train)
-
-regfit.best <- regsubsets(Salary~., data=Hitters[train,], nvmax=19)
-test.mat <- model.matrix(Salary~., data=Hitters[test,])
-val.errors <- rep(NA,19)
-for (i in 1:19) {
-    coefi <- coef(regfit.best,id=i)
-    pred <- test.mat[,names(coefi)]%*%coefi
-    val.errors[i] <- mean((Hitters$Salary[test]-pred)^2)
-}
-val.errors
-which.min(val.errors)
-coef(regfit.best,10)
-
 predict.regsubsets <- function(object,newdata,id,...) {
     form <- as.formula(object$call[[2]])
     mat <- model.matrix(form, newdata)
@@ -58,118 +9,13 @@ predict.regsubsets <- function(object,newdata,id,...) {
     mat[,xvars]%*%coefi
 }
 
-regfit.best <- regsubsets(Salary~.,data=Hitters,nvmax=19)
-coef(regfit.best,10)
-k <- 10
-set.seed(1)
-folds <- sample(1:k, nrow(Hitters), replace=TRUE)
-cv.errors <- matrix(NA, k, 19, dimnames=list(NULL, paste(1:19)))
-
-for (j in 1:k) {
-    best.fit <- regsubsets(Salary~., data=Hitters[folds!=j,], nvmax=19)
-    for (i in 1:19) {
-        pred <- predict(best.fit, Hitters[folds==j,], id=i)
-        cv.errors[j,i] <- mean((Hitters$Salary[folds==j]-pred)^2)
-    }
-}
-mean.cv.errors <- apply(cv.errors, 2, mean)
-mean.cv.errors
-par(mfrow=c(1,1))
-plot(mean.cv.errors, type='b')
-
-reg.best <- regsubsets(Salary~.,data=Hitters,nvmax=19)
-coef(reg.best,11)
-# Ha ha.  There it is, the best model!
-
-# Ridge Regression
 install.packages('glmnet')
 library(glmnet)
-x <- model.matrix(Salary~.,Hitters)[,-1]
-y <- Hitters$Salary
-
-grid <- 10^seq(10, -2, length=100)
-ridge.mod <- glmnet(x, y, alpha=0, lambda=grid)
-dim(coef(ridge.mod))
-ridge.mod$lambda[50]
-coef(ridge.mod)[,50]
-sqrt(sum(coef(ridge.mod)[-1,50]^2))
-
-ridge.mod$lambda[60]
-coef(ridge.mod)[,60]
-sqrt(sum(coef(ridge.mod)[-1,60]^2))
-# I am definitely having that linear algebra feeling
-
-predict(ridge.mod, s=50, type="coefficients")[1:20,]
-set.seed(1)
-train <- sample(1:nrow(x), nrow(x)/2)
-test <- (-train)
-y.test <- y[test]
-
-ridge.mod <- glmnet(x[train,], y[train], alpha=0, lambda=grid, thresh=1e-12)
-ridge.pred <- predict(ridge.mod, s=4, newx=x[test,])
-mean((ridge.pred-y.test)^2)
-mean((mean(y[train])-y.test)^2)
-ridge.pred <- predict(ridge.mod, s=1e10, newx=x[test,])
-mean((ridge.pred-y.test)^2)
-
-ridge.pred <- predict(ridge.mod, s=0, newx=x[test,], exact=T)
-# I get an error when I do this ?
-
-set.seed(1)
-train <- sample(1:nrow(x), nrow(x)/2)
-test <- (-train)
-y.test <- y[test]
-cv.out <- cv.glmnet(x[train,], y[train], alpha=0)
-plot(cv.out)
-bestlam <- cv.out$lambda.min
-bestlam
-# huh? this doesn't match the book either
-ridge.pred <- predict(ridge.mod, s=bestlam, newx=x[test,])
-mean((ridge.pred - y.test)^2)
-out <- glmnet(x,y,alpha=0)
-predict(out, type="coefficients", s=bestlam)[1:20,]
-
-lasso.mod <- glmnet(x[train,], y[train], alpha=1, lambda=grid)
-plot(lasso.mod)
-
-set.seed(1)
-cv.out <- cv.glmnet(x[train,], y[train], alpha=1)
-plot(cv.out)
-bestlam <- cv.out$lambda.min
-lasso.pred <- predict(lasso.mod, s=bestlam, newx=x[test,])
-mean((lasso.pred - y.test)^2)
-out <- glmnet(x, y, alpha=1, lambda=grid)
-lasso.coef <- predict(out, type="coefficients", s=bestlam)[1:20,]
-lasso.coef
-# ha ha nope!  I'm still getting every coefficient.
-lasso.coef[lasso.coef!=0]
 
 install.packages('pls')
 library(pls)
-set.seed(2)
 
-pcr.fit <- pcr(Salary~., data=Hitters, scale=TRUE, validation="CV")
-summary(pcr.fit)
-validationplot(pcr.fit, val.type="MSEP")
-
-set.seed(2)
-pcr.fit <- pcr(Salary~., data=Hitters, subset=train, scale=TRUE, validation="CV")
-validationplot(pcr.fit, val.type="MSEP")
-pcr.pred <- predict(pcr.fit, x[test,], ncomp=7)
-mean((pcr.pred - y.test)^2)
-pcr.fit <- pcr(y~x, scale=TRUE, ncomp=7)
-summary(pcr.fit)
-
-set.seed(1)
-pls.fit <- plsr(Salary~., data=Hitters, subset=train, scale=TRUE, validation="CV")
-summary(pls.fit)
-pls.pred <- predict(pls.fit, x[test,], ncomp=2)
-mean((pls.pred - y.test)^2)
-pls.fit <- plsr(Salary~., data=Hitters, scale=TRUE, ncomp=2)
-summary(pls.fit)
-
-
-# 8
+# 8.
 # a)
 set.seed(1)
 X <- rnorm(100)
@@ -211,6 +57,7 @@ graphics.off()
 # e)
 set.seed(1)
 x_data <- poly(X,10,simple=TRUE)
+
 lasso.model <- glmnet(x_data, Y, alpha=1)
 plot(lasso.model)
 
@@ -218,11 +65,10 @@ cv.model <- cv.glmnet(x_data, Y, alpha=1)
 cv.model$lambda.min
 # [1] 0.03266677
 plot(cv.model)
-out <- glmnet(x_data, Y, alpha=1)
-lasso.predictions <- predict(out, type="coefficients", s=cv.model$lambda.min)
+lasso.predictions <- predict(lasso.model, type="coefficients", s=cv.model$lambda.min)
 lasso.predictions
 # The resulting model has 7 variables with the following coefficient estimates:
-11 x 1 sparse Matrix of class "dgCMatrix"
+## 11 x 1 sparse Matrix of class "dgCMatrix"
 ##                        1
 ## (Intercept)  4.243526722
 ## 1           86.439147362
@@ -235,3 +81,50 @@ lasso.predictions
 ## 8            .
 ## 9            .
 ## 10          -0.624561786
+
+# f)
+new_Y <- 1+7*(X^7)+noise
+new_data <- data.frame(X, new_Y)
+new_regfit <- regsubsets(Y~X, new_data, nvmax=10)
+
+par(mfrow=c(2,2))
+plot(new_regfit, scale="adjr2")
+plot(new_regfit, scale="Cp")
+plot(new_regfit, scale="bic")
+coef(new_regfit, 3)
+ ## (Intercept) poly(X, 10, raw = TRUE)1 poly(X, 10, raw = TRUE)2
+ ## 1.061507                 1.975280                 2.876209
+
+graphics.off()
+
+set.seed(1)
+lasso.model <- glmnet(x_data, new_Y, alpha=1)
+cv.model <- cv.glmnet(x_data, new_Y, alpha=1)
+cv.model$lambda.min
+## [1] 7.507068
+lasso.predictions <- predict(lasso.model, type="coefficients", s=cv.model$lambda.min)
+lasso.predictions
+## 11 x 1 sparse Matrix of class "dgCMatrix"
+##                      1
+## (Intercept)   30.50469
+## 1           2273.53630
+## 2            898.90729
+## 3           3026.85923
+## 4            494.73254
+## 5           1165.73243
+## 6             34.15447
+## 7            127.60532
+## 8              .
+## 9              .
+## 10             .
+
+# 9.
+# a)
+library(ISLR)
+
+set.seed(1)
+training_college <- sample(c(TRUE, FALSE), nrow(College), TRUE)
+test_college <- (!training_college)
+
+# b)
+lm.fit <- lm(Apps~.,data=College[training_college,])
